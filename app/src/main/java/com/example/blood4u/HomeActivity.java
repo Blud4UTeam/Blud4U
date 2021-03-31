63fd0e232082e2ae0ef181e72ff2df9b0c0ce9df
Your Name <you@example.com>
2021-03-31 17:58:55 +0700

    Revert "Adding elements to drawerbar & enabling info button"
    This reverts commit ebf3b78990d04b07ef83c9184eec5a6b0835f551.

--- app/src/main/java/com/example/blood4u/HomeActivity.java
+++ app/src/main/java/com/example/blood4u/HomeActivity.java
@@ -1,8 +1,5 @@
 package com.example.blood4u;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -11,20 +8,24 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
-    androidx.drawerlayout.widget.DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button button = findViewById(R.id.dwb);
         Button buttonInfo = findViewById(R.id.info);
-        drawerLayout = findViewById(R.id.drawer_layout);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
         buttonInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,4 +34,5 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
+
 }
